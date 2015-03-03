4f5f677bc3c9d8c4eb5de898e42fa404a92a820b
alejo80805 <alejo80805@hotmail.com>
2015-03-02 22:37:58 -0500

    Initial commit

---
+++ app/src/main/java/com/example/alejandrorodriguez/pruebainicial/TabContainer.java
@@ -0,0 +1,36 @@
+package com.example.alejandrorodriguez.pruebainicial;
+
+import android.app.TabActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TabHost;
+import android.widget.TabHost;
+
+
+
+public class TabContainer extends TabActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tab_container);
+
+
+        TabHost tabHost = getTabHost();
+
+        Intent intent = new Intent(this, MainActivity.class);
+        TabHost.TabSpec spec = tabHost.newTabSpec("Tab 1");
+        spec.setIndicator("Tab 11");
+        spec.setContent(intent);
+        tabHost.addTab(spec);
+
+
+        intent = new Intent(this, MainActivity2.class);
+        spec = tabHost.newTabSpec("Tab 2");
+        spec.setIndicator("Tab 22");
+        spec.setContent(intent);
+        tabHost.addTab(spec);
+
+
+    }
+}
